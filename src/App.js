6e185ae8877d2ab6ef5a08ba269b5d77677f6f73
Simon Peter Wanyama <45794648+realespee@users.noreply.github.com>
2022-02-05 09:07:48 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
+import muele from './muele.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={muele} className="App-logo" alt="logo" />
         <p>
           What you are looking for will all be here soon!
         </p>
