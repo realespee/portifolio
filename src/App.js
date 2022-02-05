cfbaa89aa9efd4cacb9495107ba71db4784aca52
Simon Peter Wanyama <45794648+realespee@users.noreply.github.com>
2022-02-05 08:57:54 +0300

    update home

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          What you are looking for will all be here soon!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Cheers!
         </a>
       </header>
     </div>
